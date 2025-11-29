05ec42447116995218bbd7ece8566e1b678409dc
Timothy <madindoustephen8@gmail.com>
2025-11-30 02:54:46 +0300

    swagger and api mount

---
+++ ecommerce-backend/ecommerce-backend/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(title="ecommerce-backend API", default_version="v1"),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("ecommerce.urls")),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger-ui"),
+    path("graphql/", include("graphene_django.urls")),
+]
