0766a9478f0077230cd06fc6d31b56cfa439176d
Timothy <madindoustephen8@gmail.com>
2025-11-30 03:12:41 +0300

    adding view sets

---
+++ ecommerce-backend/ecommerce-backend/views.py
@@ -0,0 +1,25 @@
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product, Category, Cart, Order
+from .serializers import ProductSerializer, CategorySerializer, CartSerializer, OrderSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by("-created_at")
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ["category", "vendor", "is_active"]
+    search_fields = ["name","slug","sku"]
+    ordering_fields = ["price","created_at"]
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
